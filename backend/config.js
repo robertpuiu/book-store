cf230eda744b32c54cb00e0589683399d31924c9
puiurobert9441 <puiurobert9441@yahoo.com>
2023-09-22 15:33:36 +0300

    post, get by id, get all books

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 5554;
+export const PORT = 5555;
 
 export const mongodbURL = `mongodb+srv://puiur:VZazYDqXL3YgN0zO@bookstore.ycbnyv6.mongodb.net/books-collection?retryWrites=true&w=majority`;
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,67 @@
 import express from 'express';
 import { PORT, mongodbURL } from '../backend/config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   return res.status(233).send(` saluy`);
+});
+
+app.post('/books', async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
+
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.find({});
+
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
+
+app.get('/books/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({
+      message: error.message,
+    });
+  }
 });
 
 mongoose
