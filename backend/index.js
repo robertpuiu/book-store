9e0d6974cd8b09495feb3eb1c97225a9fae5f842
puiurobert9441 <puiurobert9441@yahoo.com>
2023-09-22 22:43:08 +0300

    put and delete

--- backend/index.js
+++ backend/index.js
@@ -64,6 +64,45 @@ app.get('/books/:id', async (req, res) => {
   }
 });
 
+app.put('/books/:id', async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    return res.status(200).send({ message: 'book updated successfully' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    return res.status(200).send({ message: 'book deleted successfully' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
 mongoose
   .connect(mongodbURL)
   .then(() => {
