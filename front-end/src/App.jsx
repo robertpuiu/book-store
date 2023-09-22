5d2a626ec78386759d58c36b084ca0c0b7506ccc
puiurobert9441 <puiurobert9441@yahoo.com>
2023-09-23 02:30:23 +0300

    final UI

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateBook from './pages/CreateBook';
-import DeleteBook from './pages/DeleteBook';
-import EditBook from './pages/EditBook';
 import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="" element={<CreateBook />} />
-      <Route path="" element={<DeleteBook />} />
-      <Route path="" element={<EditBook />} />
-      <Route path="" element={<Show />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   );
 };
